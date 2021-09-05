07f04a1c61821c8780ca1267927d3c633f04c53d
Ibrahim Khalid <satellite-301@outlook.com>
2021-09-05 15:02:43 +0300

    more stuff

--- src/main.js
+++ src/main.js
@@ -1,17 +1,8 @@
 import { GA } from './ga_minTest.js'
-import { buttons } from './main/mainSetUp/initBottomPanel.js'
-import { initLayers, uiLayer } from './main/mainSetUp/initLayers.js'
-import { canvas, initCanvasEvents } from './main/mainSetUp/initCanvas.js'
-import { initMap } from './main/mainSetUp/initMap.js'
-import { debugShape } from '../debug.js'
+import { canvas, initCanvasEvents } from './initCanvas.js'
+// import { debugShape } from '../debug.js'
 
-// export const mainCanvas = {
-//   get width() { return 380 },
-//   get height() { return 540}
-//   // get width() { return canvas.width },
-//   // get height() { return canvas.height}
-// }
-
+export let uiLayer, layer2, buttons = []
 
 const objects = [
   '🎮',
@@ -47,8 +38,7 @@ const nonSolids = [
   '🍰',
   '🐁',
   '🐠',
-  '🐓',
-  '🐌'
+  '🐓'
 ]
 
 const uiStuff = [
@@ -76,15 +66,14 @@ const products = []
 
 let pushed = false
 
+let stackSize = 0
+
 let inserted = 0
 
 const surfaceWidth = 414
 const surfaceHeight = 812
 const cellSize = 73
-const PI = Math.PI
-
-
-let removeElementIndex
+// const PI = Math.PI
 
 const currentAction = {
   placingBuilding: false,
@@ -114,36 +103,29 @@ let bloodDrops = []
 let fadeOuts = []
 let miners = []
 
-let removed = false
+let toBeSmelted = []
+let smelting = false
 
 let debug1
 let debug2
 let debug3
 
 
+let smelter
+let randomElement
+
+let finishedProduct
+
+let smeltTime, currentSmeltTime
+let smelterProgressBar
+
+
+let space
+
 export let uiElements = []
 
-
-g = GA.create(mainMenu)
+g = GA.create(setup)
 g.start()
-
-// function setSize(element, p1, p2) {
-//   element.width = mainCanvas.width * p1
-//   element.height = mainCanvas.height * p2
-// }
-
-// export function setPos(element, p1, p2, xOffset = 0, yOffset = 0) {
-//   element.x = mainCanvas.width * p1 + xOffset
-//   element.y = mainCanvas.height * p2 + yOffset
-// }
-
-// export function adjustElement(e, w, h, tx, ty) {
-//   setSize(e, w, h)
-//   if (e.text) {
-//     e.text.x = e.width * tx
-//     e.text.y = e.height * ty
-//   }
-// }
 
 function buttonPress(b) {
   b.f = '#FFF'
@@ -162,16 +144,10 @@ function removeElement(n) {
 }
 
 function addElement(x = 10, y = buttons[0].y - 50) {
-  const randomElement = nonSolids[g.randomNum(0, nonSolids.length)]
-  const t = g.makeText(uiLayer, randomElement, 18, '#FFF', x, y)
-  // t.adjust = () => {
-
-  //   // t.y = uiElements[0]. y - 50
-  //   // if (DynamicSize) adjustElement(button, width, height, textX, textY)
-  //   // if (DynamicPos) setPos(button, xPer, yPer, xOff, -button.height)
-  // }
+  if (Math.random() > .5) randomElement = objects[g.randomNum(0, objects.length)]
+  else randomElement = nonSolids[g.randomNum(0, nonSolids.length)]
+  const t = g.makeText(uiLayer, randomElement, 18, 0, x, y)
   mainBelt.unshift(t)
-  // uiElements.push(t)
 }
 
 function moveElements(n = 0) {
@@ -192,6 +168,7 @@ function moveElementsNOW() {
     for (const item of movingElements) {
       item.x += moveSteps
     }
+    shiftAmount += moveSteps
     if (shiftAmount < maxShift) {
       g.wait(1, () => moveElementsNOW())
       
@@ -215,34 +192,103 @@ function moveElementsNOW() {
         movingElements.length = 0
       }
     }
-    shiftAmount += moveSteps
+    
   // }
 }
 
-function mainMenu(){
-  initCanvasEvents()
-  initLayers()
-  // menu = g.simpleButton('>', 0, 1, 0, .4, .4, () => {
-  //   // if (!mainBelt.length) addElement()
-  //   // else moveElements()
-  //   // buttonPress(menu)
-  // }, .15, .1)
+const productsMaxMove = 75
+let productsMoveAmount = 0
 
+function moveToSmelter() {
+  if (productsMoveAmount < productsMaxMove) {
+    productsMoveAmount += 5
+    toBeSmelted.forEach(p => p.x -= 5)
+    g.wait(5, () => {
+      moveToSmelter()
+    })
+  } else {
+    productsMoveAmount = 0
+    startSmelting()
+  }
+}
+
+function startSmelting() {
+
+  smeltTime = g.randomNum(50, 100)
+  currentSmeltTime = 0
+  smelter.doneBar.visible = false
+  
+
+
+
+  smelt()
+
+  toBeSmelted.forEach(p => g.remove(p))
+  toBeSmelted.length = 0
+  smelting = false
+}
+
+function smelt() {
+  if (currentSmeltTime < smeltTime) {
+    currentSmeltTime += 1
+    smelter.bar.width = (currentSmeltTime / smeltTime) * 60
+    g.wait(5, () => smelt())
+  } else {
+    smelter.doneBar.visible = true
+    leftAmount = 0
+    upAmount = 0
+    space.visible = true
+    eject()
+  }
+}
+
+const leftSteps = 115
+const upSteps = 500
+let leftAmount, upAmount
+
+function eject() {
+  if (leftAmount < leftSteps) {
+    leftAmount += 5
+    space.x -= 5
+    g.wait(1, eject)
+  } else {
+    g.wait(500, moveUp)
+    
+  }
+}
+
+
+function moveUp() {
+  if (upAmount < upSteps) {
+    upAmount += 7
+    space.y -= 7
+    g.wait(1, moveUp)
+  } else {
+    space.visible = false
+    space.x = 310
+    space.y = 75
+  }
+}
+
+
+
+
+function initButtons() {
   const buttonsHeight = 540
   
-  const r1 = g.simpleButton('discard', 4, buttonsHeight, 8, 10, () => {
+  const r1 = g.simpleButton('Discard', 4, buttonsHeight, 8, 10, () => {
     removeElement(3)
     moveElements(2)
     buttonPress(r1)
   })
 
-  const r2 = g.simpleButton('remove', r1.x + r1.width + 4, buttonsHeight, 8, 10, () => {
+  const r2 = g.simpleButton('Remove', r1.x + r1.width + 4, buttonsHeight, 8, 10, () => {
     removeElement(2)
     moveElements(1)
     buttonPress(r2)
   })
 
-  const r3 = g.simpleButton('delete', r2.x + r2.width + 4, buttonsHeight, 8, 10, () => {
+  const r3 = g.simpleButton('Delete', r2.x + r2.width + 4, buttonsHeight, 8, 10, () => {
     if (elementsCanMove && !pushed) {
       mainBelt.pop().visible = false
       moveElements(0)
@@ -250,87 +296,102 @@ function mainMenu(){
     buttonPress(r3)
   })
 
-  const ok = g.simpleButton('OK', r3.x + r3.width + 4, buttonsHeight, 8, 10, () => {
-
-    if (elementsCanMove && !pushed) {
-      pushed = true
-
-      const l = mainBelt.length
-      for (let i = 1; i < 4; i++) {
-        const item = mainBelt[l - i]
-        g.removeItem(mainBelt, item)
-        products.push(item)
+  const push = g.simpleButton('Push', r3.x + r3.width + 4, buttonsHeight, 8, 10, () => {
+    if (stackSize < 10) {
+      if (elementsCanMove && !pushed) {
+        stackSize += 1
+        pushed = true
+        const l = mainBelt.length
+        for (let i = 1; i < 4; i++) {
+          const item = mainBelt[l - i]
+          g.removeItem(mainBelt, item)
+          products.push(item)
+        }
+        products.forEach(p => p.y -= 40)
+        moveElements()
       }
-      
-      
-      products.forEach(p => p.y -= 40)
-      moveElements()
+    }
+    buttonPress(push)
+  })
+  
+  const smelt = g.simpleButton('Smelt', push.x + push.width + 4, buttonsHeight, 8, 10, () => {    
+    if (stackSize == 10) {
+      if (!smelting) {
+
+        smelting = true
+        products.forEach(p => toBeSmelted.push(p))
+
+        moveToSmelter()
+        stackSize = 0
+        products.length = 0
+      }
+
     }
     
-    buttonPress(ok)
+    
+    buttonPress(smelt)
   })
-
-  // const squareWidth = 100
-  // const squareHeight = 100
-  
-  // uiElements.forEach(e => e.adjust())
-
-
-
-  for (let i = 0; i < 4; i++) {
-    addElement(10 + (25 * (3 - i)))
-  }
-  
-  setup()
 }
 
-debug1 = g.makeText(g.stage, 'text 1', 20, '#FFF')
-debug2 = g.makeText(g.stage, 'text 2', 20, '#FFF', 0, 20)
-debug3 = g.makeText(g.stage, 'text 2', 20, '#FFF', 0, 40)
 
 function setup(){
-  // initMap()
-  g.state = play
+
+  debug1 = g.makeText(g.stage, 'text 1', 20, '#FFF')
+  debug2 = g.makeText(g.stage, 'text 2', 20, '#FFF', 0, 20)
+  debug3 = g.makeText(g.stage, 'text 3', 20, '#FFF', 0, 40)
+
+  initCanvasEvents()
+  uiLayer = g.group()
+  layer2 = g.group()
+
+  initButtons()
+
+  
+  const packaging = g.rectangle(100, 50, '#333', 1, 180, 0)
+  layer2.addChild(packaging)
+
+  smelter = g.rectangle(80, 410, '#222', 2, 305, 70)
+  layer2.addChild(smelter)
+
+  const smelterBarEmpty = g.rectangle(60, 20, '#000', 0, 315, 440)
+  layer2.addChild(smelterBarEmpty)
+
+  smelter.bar = g.rectangle(60, 20, '#ff0', 0, 315, 440)
+  layer2.addChild(smelter.bar)
+  
+
+  smelter.doneBar = g.rectangle(60, 20, '#0f0', 0, 315, 440)
+  layer2.addChild(smelter.doneBar)
+  // smelter.doneBar.visible = false
+
+  
+  space = g.rectangle(70, 400, '#777', 2, 310, 75)
+  space.visible = false
+  uiLayer.addChild(space)
+
+
+  const beltSize = 17
+  for (let i = 0; i <= beltSize; i++) {
+    addElement(10 + (25 * (beltSize - i)))
+  }
+
 }
 
 function play(){
-
-  debug1.content = `${window.innerWidth}`
-  debug2.content = `${window.innerHeight}`
-
-
+  debug1.content = `stack = ${stackSize}`
+  debug2.content = `products = ${products.length}`
   debug3.content = `canvas ${canvas.width} , ${canvas.height}`
+  // debug1.content = `${window.innerWidth}`
+  // debug2.content = `${window.innerHeight}`
+  // debug3.content = `canvas ${canvas.width} , ${canvas.height}`
 
-  // if (mainBelt.length < 10) {
-  //   addElement()
-  // }
-  // theText.content = `
-  // ${g.pointer.x.toFixed(2)}
-  // ${g.pointer.y.toFixed(2)}
-  // `
-
-  // theText2.content = `
-  // ${menu.gx.toFixed(2)}
-  // ${menu.gy.toFixed(2)}
-  // `
-
-  // if (g.hitTestPoint(g.pointer, menu)) {
-  //   menu.f = '#FFF'
-  //   g.wait(50, () => {
-  //     menu.f = '#800'
-  //   })
-  //   console.log('pointer = ', g.pointer.x.toFixed(2), g.pointer.y.toFixed(2))
-  //   console.log('button = ', menu.gx, menu.gy)
-  // }
-
-  
 
 }
 
 export { 
   g,
   K,
-  PI,
+  // PI,
   currentAction,
   surfaceHeight,
   surfaceWidth,
