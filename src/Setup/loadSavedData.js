026959d720b748c3e982c5188bb89651a9c5ac2f
Ibrahim Khalid <satellite-301@outlook.com>
2021-09-11 16:10:40 +0300

    load from / save to local storage

---
+++ src/Setup/loadSavedData.js
@@ -0,0 +1,27 @@
+export const stats = {}
+export let data
+export let LS = localStorage
+export const statsKey = 'SpaceBarMaker2389JS13K2021'
+export function loadSavedData() {
+    try {
+      data = JSON.parse(LS[statsKey])
+      if (data.currentCash != undefined) {
+        stats.currentCash = data.currentCash
+        stats.displayedCash = data.currentCash
+        stats.repairCost = data.repairCost
+        stats.machineHealth = data.machineHealth
+      } else {
+        setData()
+      }
+    } catch (error) {
+      setData()
+    }
+}
+
+function setData() {
+  stats.currentCash = 1000
+  stats.repairCost = 0
+  stats.machineHealth = 300
+  data = {...stats}
+  stats.displayedCash = stats.currentCash
+}
